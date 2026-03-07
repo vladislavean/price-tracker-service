c57c48bfb7c099ea0fe73539f5bd85215a92acf2
Vladislav Ananev <v_ananev@head-point.ru>
2026-03-07 16:31:31 +0300

    реализация остальных бирж

--- src/internal/exchange-clients/bybit_client.go
+++ src/internal/exchange-clients/bybit_client.go
@@ -1,6 +1,26 @@
 package exchange_clients
 
-import "github.com/shopspring/decimal"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/shopspring/decimal"
+)
+
+type ByBitGetPriceResultList struct {
+	List []ByBitGetPriceResult `json:"list"`
+}
+
+type ByBitGetPriceResult struct {
+	LastPrice string `json:"lastPrice"`
+}
+
+type ByBitGetPriceResponse struct {
+	RetCode int                     `json:"retCode"`
+	RetMsg  string                  `json:"retMsg"`
+	Result  ByBitGetPriceResultList `json:"result"`
+}
 
 type ByBitGetPriceClientImpl struct {
 }
@@ -10,7 +30,21 @@ func NewByBitGetPriceClientImpl() *ByBitGetPriceClientImpl {
 }
 
 func (r *ByBitGetPriceClientImpl) GetExchangePrice(pairName string) (decimal.Decimal, error) {
-	panic("implement me")
+	baseUrl := fmt.Sprintf("https://api-testnet.bybit.com/v5/market/tickers?category=spot&symbol=%s", pairName)
+
+	resp, err := http.Get(baseUrl)
+	if err != nil {
+		return decimal.Zero, err
+	}
+	defer resp.Body.Close()
+
+	var result ByBitGetPriceResponse
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return decimal.Zero, err
+	}
+
+	return decimal.NewFromString(result.Result.List[0].LastPrice)
 }
 
 func (r *ByBitGetPriceClientImpl) GetName() string {
--- src/internal/exchange-clients/okx_client.go
+++ src/internal/exchange-clients/okx_client.go
@@ -1,6 +1,22 @@
 package exchange_clients
 
-import "github.com/shopspring/decimal"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/shopspring/decimal"
+)
+
+type OkxGetPriceClientPriceResponse struct {
+	Last string `json:"last"`
+}
+
+type OkxGetPriceClientResponse struct {
+	Code string                           `json:"code"`
+	Msg  string                           `json:"msg"`
+	Data []OkxGetPriceClientPriceResponse `json:"data"`
+}
 
 type OkxGetPriceClientImpl struct{}
 
@@ -9,7 +25,23 @@ func NewOkxGetPriceClientImpl() *OkxGetPriceClientImpl {
 }
 
 func (r *OkxGetPriceClientImpl) GetExchangePrice(pairName string) (decimal.Decimal, error) {
-	panic("implement me")
+	baseUrl := fmt.Sprintf("https://www.okx.com/api/v5/market/ticker?instId=%s", pairName)
+
+	resp, err := http.Get(baseUrl)
+	if err != nil {
+		return decimal.Zero, err
+	}
+
+	defer resp.Body.Close()
+
+	var data OkxGetPriceClientResponse
+	err = json.NewDecoder(resp.Body).Decode(&data)
+
+	if err != nil {
+		return decimal.Zero, err
+	}
+
+	return decimal.NewFromString(data.Data[0].Last)
 }
 
 func (r *OkxGetPriceClientImpl) GetName() string {
